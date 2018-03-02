e34ca88649abb7a0b9a142f32156d3b31a0a80b1
omelendrez <omar.melendrez@gmail.com>
2018-03-01 23:06:50 -0300

    Local or Heroku

--- src/components/http-common.js
+++ src/components/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // http://localhost:3010/
 // https://pure-ridge-51583.herokuapp.com/
 const HTTP = axios.create({
-  baseURL: 'https://pure-ridge-51583.herokuapp.com/',
+  baseURL: 'http://localhost:3010/',
   headers: {
     // Authorization: 'Bearer {token}'
   }
