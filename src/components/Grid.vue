47983d41022c0719a6274d2ac463cff87ec8e9f6
omelendrez <omar.melendrez@gmail.com>
2018-11-07 23:00:28 -0300

    Refactor to simplify code

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -121,10 +121,11 @@
 </template>
 
 <script>
-import Store from '../store/store'
+import Store from '@/store/store'
 import Header from './Header'
 import Positions from './lib/Positions'
-import { hoursLimit } from './../store/constants'
+import { hoursLimit } from '@/store/constants'
+const fields = require('@/utils/fields.json').grid
 
 export default {
   name: 'Grid',
@@ -135,121 +136,7 @@ export default {
       errorMessage: '',
       timeoffAlert: false,
       timeoffMessage: '',
-      fields: [
-        {
-          key: 'fullName',
-          label: 'Empleado',
-          class: 'p-0 py-1 full-name',
-          thStyle: {
-            width: '160px'
-          }
-        },
-        {
-          key: 'h06',
-          label: '06',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h07',
-          label: '07',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h08',
-          label: '08',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h09',
-          label: '09',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h10',
-          label: '10',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h11',
-          label: '11',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h12',
-          label: '12',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h13',
-          label: '13',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h14',
-          label: '14',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h15',
-          label: '15',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h16',
-          label: '16',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h17',
-          label: '17',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h18',
-          label: '18',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h19',
-          label: '19',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h20',
-          label: '20',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h21',
-          label: '21',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h22',
-          label: '22',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h23',
-          label: '23',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h24',
-          label: '24',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'h25',
-          label: '01',
-          class: 'text-center p-0 py-1'
-        },
-        {
-          key: 'hours',
-          label: 'Horas',
-          class: 'text-center p-0 py-1'
-        }
-      ],
+      fields: fields,
       scheduleRows: [],
       positionRows: [],
       recordData: {},
