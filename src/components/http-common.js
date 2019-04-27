4753e9c738d5a02f4daa07cda54a6c1c7125d382
omelendrez <omar.melendrez@gmail.com>
2019-04-27 20:20:43 -0300

    Change url

--- src/components/http-common.js
+++ src/components/http-common.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 // http://localhost:3010/
 // https://escng-schedule.herokuapp.com/
+// TODO - Changes only for iPage
 const HTTP = axios.create({
-  baseURL: 'http://localhost:3010/',
+  baseURL: 'https://escng-schedule.herokuapp.com/',
   headers: {
     // Authorization: 'Bearer {token}'
   }
