ae6c49efae3d0c5cadaa1904a5b21553b67af1bc
Omar Melendrez <omar.melendrez@gmail.com>
2022-08-26 20:08:47 -0300

    Change backend api

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  VUE_APP_ENDPOINT: '"https://turnera-api.herokuapp.com/"'
+  VUE_APP_ENDPOINT: '"https://turnera-api.up.railway.app/"'
 }
+
+// https://turnera-api.up.railway.app/
+// https://turneta-api.onrender.com/
--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 // http://localhost:3010/
 
-console.log(process.env)
 const HTTP = axios.create({
   baseURL: process.env.VUE_APP_ENDPOINT,
   headers: {
