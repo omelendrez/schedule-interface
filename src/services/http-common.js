ab36ba7eb44a901093895f8357113e0a0747ce35
Omar Melendrez <omar.melendrez@gmail.com>
2022-08-27 12:09:57 -0300

    Clean code

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import { LOCAL_STORAGE_VARS, getPersistedValue } from '../utils'
-// http://localhost:3010/
 
 const token = getPersistedValue(LOCAL_STORAGE_VARS.TOKEN)
+
 const HTTP = axios.create({
   baseURL: process.env.VUE_APP_ENDPOINT
 })
